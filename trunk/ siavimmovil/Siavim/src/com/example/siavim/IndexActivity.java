addc1deaf4d7f307f252faecc00d056918c0f188
ernestoandrade86@gmail.com <ernestoandrade86@gmail.com@ae6044f3-992d-92a3-67c0-c27d0e535244>
2014-12-03 03:48:45 +0000

    Se agrega proyecto movil.

---
+++ trunk/ siavimmovil/Siavim/src/com/example/siavim/IndexActivity.java
@@ -0,0 +1,16 @@
+package com.example.siavim;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.dsadsa.R;
+
+public class IndexActivity extends Activity {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setTitle("SIAVIM");
+		setContentView(R.layout.index_activity);
+	}
+}
---
+++ trunk/ siavimmovil/Siavim/src/com/example/siavim/Login.java
@@ -0,0 +1,21 @@
+package com.example.siavim;
+
+public class Login {
+
+	public String usuario;
+	public String clave;
+	
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getClave() {
+		return clave;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	
+}
