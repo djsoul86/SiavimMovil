eebb0a2b5bee563d0a1a74b9b2716644077382f1
ernestoandrade86@gmail.com <ernestoandrade86@gmail.com@ae6044f3-992d-92a3-67c0-c27d0e535244>
2014-12-16 04:28:16 +0000

    se agrega activity para modificar usuario.

---  siavimmovil/Siavim/gen/com/example/dsadsa/R.java
+++  siavimmovil/Siavim/gen/com/example/dsadsa/R.java
@@ -25,24 +25,39 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int LabelApellido=0x7f080005;
+        public static final int LabelEmail=0x7f080007;
+        public static final int LabelPassword=0x7f08000b;
+        public static final int LabelTelefono=0x7f080009;
+        public static final int LabelUser=0x7f080003;
+        public static final int action_settings=0x7f080010;
+        public static final int btGuardar=0x7f08000d;
         public static final int btIngresar=0x7f080002;
-        public static final int btModificar=0x7f080003;
+        public static final int btModificar=0x7f08000f;
         public static final int etContrasena=0x7f080001;
         public static final int etUsuario=0x7f080000;
+        public static final int tvApellidoUsuario=0x7f080006;
+        public static final int tvEmailUsuario=0x7f080008;
+        public static final int tvNombre=0x7f08000e;
+        public static final int tvNombreUsuario=0x7f080004;
+        public static final int tvPasswordUsuario=0x7f08000c;
+        public static final int tvTelefonoUsuario=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int activity_prueba=0x7f030001;
-        public static final int index_activity=0x7f030002;
+        public static final int activity_modificar_user=0x7f030001;
+        public static final int activity_prueba=0x7f030002;
+        public static final int index_activity=0x7f030003;
     }
     public static final class menu {
-        public static final int prueba=0x7f070000;
+        public static final int modificar_user=0x7f070000;
+        public static final int prueba=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050003;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int title_activity_modificar_user=0x7f050004;
         public static final int title_activity_prueba=0x7f050002;
     }
     public static final class style {
